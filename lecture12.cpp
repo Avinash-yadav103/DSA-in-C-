9566782eef8082465ea4e406c91a08885b3f9aac
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-05-30 05:54:51 +0530

    lec12 add

---
+++ lecture12.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[],int size,int key){
+    int start =0;
+    int end =size-1;
+    
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+
+        if(key>arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end =mid-1;
+        }
+        mid = (start+end)/2;
+    }
+
+    return -1;
+}
+
+
+int main(){
+
+    int even[6] = {};
+    // int odd[5] = {};
+
+    int search = binarySearch(even,6,10);
+    cout<<"The index of key is "<<search;
+
+    return 0;
+}
