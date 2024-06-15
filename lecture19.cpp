e12473b49922a9093ed0f08412d2e52618ab2010
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-06-16 04:01:43 +0530

    STL

--- lecture19.cpp
+++ lecture19.cpp
@@ -2,6 +2,12 @@
 #include<array>
 #include<vector>
 #include<deque>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<algorithm>
 using namespace std;
 
 
@@ -125,10 +131,193 @@ int deq(){
 
 }
 
+int qu(){
+    queue<int> q;
+    //To add element at the end
+    q.push(1);
+    //To remove element from the front
+    q.pop();
+    //To check the size of queue
+    q.size();
+    //To check if queue is empty or not
+    q.empty();
+    //To access the front element
+    q.front();
+    //To access the back element
+    q.back();
+    //To swap two queues
+    q.swap(q);
+    //To insert element at the end
+    q.emplace(2);
+}
+
+int li(){
+    list<int> l = {1,2,3,4,5};
+    //To add element at the end
+    l.push_back(6);
+    //To add element at the front
+    l.push_front(0);
+    //To remove element from the end
+    l.pop_back();
+    //To remove element from the front
+    l.pop_front();
+    //To check the size of list
+    l.size();
+    //To check if list is empty or not
+    l.empty();
+    //accessing the first element
+    l.front();
+    //accessing the last element
+    l.back();
+    
+
+}
+
+int stk(){
+    stack<int, deque<int>> s;
+    //To add element at the top
+    s.push(1);
+    //To remove element from the top
+    s.pop();
+    //To check the size of stack
+    s.size();
+    //To check if stack is empty or not
+    s.empty();
+    //To access the top element
+    s.top();
+    //To swap two stacks
+    s.swap(s);
+    //To insert element at the top
+    s.emplace(2);
+    
+}
+
+int pr_queue(){
+    //max-heap creation
+    priority_queue<int> maxi_pq;
+
+    //min heap creation
+    priority_queue<int,vector<int>,greater<int>> mini_pq;
+    
+    maxi_pq.push(1);
+    maxi_pq.push(7);
+    maxi_pq.push(5);
+    maxi_pq.push(4);
+
+    // Printing the numbers in the priority queue
+    while (!maxi_pq.empty()) {
+        cout << maxi_pq.top() << " ";
+        maxi_pq.pop();
+    }
+    cout << endl;
+    //Printing using for loop
+    for(int i=0;i<mini_pq.size();i++){
+        cout<<mini_pq.top()<<" ";
+        mini_pq.pop();
+    }    
+
+    mini_pq.push(1);
+    mini_pq.push(7);
+    mini_pq.push(5);
+    mini_pq.push(4);
+
+    // Printing the numbers in the priority queue
+    while (!mini_pq.empty()) {
+        cout << mini_pq.top() << " ";
+        mini_pq.pop();
+    }
+    cout << endl;
+
+    //Printing using for loop
+    for(int i=0;i<mini_pq.size();i++){
+        cout<<mini_pq.top()<<" ";
+        mini_pq.pop();
+    }
+
+}
+
+int st(){
+    set<int> s;
+    //To add element
+    s.insert(1);
+    s.insert(1);
+    s.insert(3);
+    s.insert(3);
+    s.insert(2);
+    s.insert(2);
+
+    //To remove element
+    s.erase(2);
+    //here 2 is index not the value
+
+    //To check the size of set
+    s.size();
+    
+    //To check number is present or not
+    s.count(1);
+
+    set<int>::iterator it = s.find(3);
+
+
+}
+
+int mp(){
+    map<int,string> m;
+    //To add element
+    //here 1 is key and "one" is value
+    m[1] = "one";
+    m[2] = "two";
+    //here 3 is key and "three" is value
+    m.insert({3,"three"});
+
+    //To remove element
+    m.erase(2);
+    //here 2 is index not the value
+
+    //To check the size of map
+    m.size();
+    
+    //To check number is present or not
+    m.count(1);
+
+    // map<int,int>::iterator it = m.find(3);
+    //sorted in map 
+    for(auto i:m){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+}
+
 int main(){
-    deque<int> dq = {1,2,3,4,5};
-    // cout<<dq.front();
-    //You cannot directly use std::cout with dq.begin() because dq.begin() returns an iterator, not the value itself.
-    dq.begin();
-    return 0;
+    
+    vector<int> v;
+    v.push_back(1);   
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
+
+    cout<<"Finding 6 - "<<binary_search(v.begin(),v.end(),6)<<endl;
+
+    cout<<"Lower Bound of 6 - "<<lower_bound(v.begin(),v.end(),6)-v.begin()<<endl;
+    cout<<"Upper Bound of 6 - "<<upper_bound(v.begin(),v.end(),6)-v.begin()<<endl;
+
+    int a = 5;
+    int b = 6;
+    cout<<"Max of 5 and 6 - "<<max(a,b)<<endl;
+    cout<<"Min of 5 and 6 - "<<min(a,b)<<endl;
+
+    // cout<<"Swap of 5 and 6 - "<<a<<" "<<b<<endl;
+    swap(a,b);
+    cout<<"Swap of 5 and 6 - "<<a<<" "<<b<<endl;
+
+    string s = "Hello";
+    reverse(s.begin(),s.end());
+    cout<<"Reverse of Hello - "<<s<<endl;
+
+    // Sort function
+    int arr[] = {1,3,2,5,4};
+    //This sort function is formed by intro sort
+    //It is a hybrid sorting algorithm, derived from quicksort, heapsort, and insertion sort.
+    sort(arr,arr+5);
+    printingArray(arr,5);
+    cout<<endl;
 }   
