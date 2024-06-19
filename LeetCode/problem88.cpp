1f08b63887dc0f1cec023e68923d7001f600d3e8
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-06-19 18:21:32 +0530

    notes

--- LeetCode/problem88.cpp
+++ LeetCode/problem88.cpp
@@ -3,43 +3,56 @@
 
 using namespace std;
 
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
 
     int i = 0;
     int j = 0;
     int k = 0;
-    vector<int> v;
-    v = nums1;
-    while(i<n && j<m){
+        vector<int> v;
+        v = nums1;
+        while(i<n && j<m){
         if(v[i]>nums2[j]){
-        nums1[k++] = nums2[j++];
-    }
-    else{
-        if(v[i]==0){
-        i++;
-                continue;
-            }
-            nums1[k++] = nums1[i++];
+            nums1[k++] = nums2[j++];
         }
+        else{
+            // if(v[i]==0){
+            //     i++;
+            //     continue;
+            // }
+            nums1[k++] = v[i++];
+        }}
 
         //Printing remaining elements of arr1
         while(i<n){
-            if(v[i]==0){
-                i++;
-                continue;
-            }
+            // if(v[i]==0){
+            //     i++;
+            //     continue;
+            // }
             nums1[k++]=v[i++];
         }
         //Printing remaining elements of arr2
         while(j<m){
-            nums1[k++]=v[i++];
+            nums1[k++]=nums2[j++];
         }
+
 
 }
 
 
-int main();{
+void printingVec(vector<int> v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
 
-    // merge();
-    // return 0;
+
+int main(){
+    
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    vector<int> v;
+
+    merge(nums1,3,nums2,3);
+    printingVec(v);
+    return 0;
 }
