da4262d57a0bd6cbabea15846761e040812098a7
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-09-10 16:16:00 +0530

    204

--- LeetCode/problem186.cpp
+++ LeetCode/problem186.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 void reverseWords(vector<char> &s){
-    cout<<"Hello";
+    for(int i;i<s.size();i++){
+        if(s[i]==' '){
+            cout<<"Hello";
+        }
+    }
 }
 
 int main(){
     
-    vector<char> hello;
-    
+    vector<char> hello = {'H', 'e', 'l', 'l', 'o', ' ', 't', 'h', 'e', 'r', 'e', ' ', 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'm', 'e'};
 
+    // for(int i;i<hello.size();i++){
+    //     cin>>hello[i];
+    // }
+    reverseWords(hello);
 
     return 0;
 }
