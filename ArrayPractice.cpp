5487dfa0f1a8e35526e1849940564c75d864c842
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-10-19 21:19:42 +0530

    Array

---
+++ ArrayPractice.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void Insertion(int arr[], int index, int n){
+    
+    for(int i=0;i<n;i++){
+
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+
+
+
+    return 0;
+}
