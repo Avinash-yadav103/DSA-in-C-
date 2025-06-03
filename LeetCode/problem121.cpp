5f029a0296892ce6bab822c4d429d53a422f236c
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2025-06-03 16:04:56 +0530

    prob 121 and 1

---
+++ LeetCode/problem121.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()){
+            return 0;
+        }
+        int min = prices[0];
+        // int max = 0;
+        int profit = 0;
+        int n =prices.size();
+        for(int i=0;i<n;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            if((prices[i]-min>profit)){
+                profit = prices[i]-min;
+            }
+
+        }
+        return profit;
+        // return 0;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    // Example test cases
+    vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+    cout << "Max profit for prices1: " << solution.maxProfit(prices1) << endl; // Expected: 5
+    
+    vector<int> prices2 = {7, 6, 4, 3, 1};
+    cout << "Max profit for prices2: " << solution.maxProfit(prices2) << endl; // Expected: 0
+    
+    vector<int> prices3 = {2, 4, 1};
+    cout << "Max profit for prices3: " << solution.maxProfit(prices3) << endl; // Expected: 2
+    
+    return 0;
+}
