942ffe14f0370c973fba793e37e6fe6566d406c4
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2025-10-23 20:09:21 +0530

    prob2619

---
+++ LeetCode/problem2619.js
@@ -0,0 +1,17 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    return this.length? this[this.length-1] : -1;
+    // if(this.length <3){
+    //     return -1;
+    // }
+    // else{
+    //     return this[this.length-1];
+    // }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
