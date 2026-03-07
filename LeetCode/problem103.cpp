43233827f397c94cf2af3242a56d93c13b58b153
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2026-03-07 16:35:46 +0530

    binary tree

---
+++ LeetCode/problem103.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(!root){
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool flag = true;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                int index = i;
+                if(!flag){
+                    index = size -i -1;
+                }
+                level[index] = node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            result.push_back(level);
+            flag = !flag;
+        }
+        return result;
+    }
+};
---
+++ LeetCode/problem199.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(i==size-1) ans.push_back;
+            }
+        }
+    }
+};
